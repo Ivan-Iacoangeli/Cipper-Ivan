7fdd1812482d9f4d47f20ca606d77a87eff1b213
Ivan Iacoangeli <ivan.iacoangeli19@gmail.com>
2024-04-11 14:31:30 +0200

    aggiunto middleware

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+<x-layouts.main>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-8 col-lg-6 mt-5">
+                <h1>Allora</h1>
+            </div>
+        </div>
+    </div>
+</x-layouts.main>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\CipperController;
+use App\Http\Middleware\RedirectIfAuth;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+})->middleware(RedirectIfAuth::class);
+
+Route::middleware('auth')->group(function(){
+    Route::get('/home' , [CipperController::class , 'index'])->name('home');
 });
